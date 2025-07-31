28e51e500068e2e12c190f00929a3e21e10d891a
kaourantinu <hello@corentintournier.fr>
2025-07-31 09:55:30 +0200

    add apolloclient and update react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { RouterProvider, createBrowserRouter } from "react-router";
 import App from "./App.tsx";
+import { ApolloClient, ApolloProvider, gql, InMemoryCache } from "@apollo/client";
 
-const root = document.getElementById("root");
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+  },
+  {
+    path: "/about",
+    element: <div>Route test About</div>,
+  }
+]);
 
-if (root) {
-  ReactDOM.createRoot(root).render(
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/about" element={<div>Route test About</div>} />
-      </Routes>
-    </BrowserRouter>,
+export const client = new ApolloClient({
+  cache: new InMemoryCache({
+    addTypename: false
+  }),
+  uri: "http://localhost:4005",
+});
+
+client
+  .query({
+    query: gql`
+      query TestQuery {
+        __typename
+      }
+    `
+  })
+  .then(response => console.log("Apollo Client is configured correctly:", response))
+  .catch(error => console.error("Apollo Client configuration error:", error));
+
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <ApolloProvider client={client}>
+      <RouterProvider router={router}/>
+    </ApolloProvider>
   );
-} else {
-  throw new Error('Root element not found');
-}
