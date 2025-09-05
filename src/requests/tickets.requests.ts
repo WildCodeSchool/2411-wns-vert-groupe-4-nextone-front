5d6b53fdaa94e93a728cf2fc3e41919420d43d30
kaourantinu <hello@corentintournier.fr>
2025-09-05 10:41:33 +0200

    wip

--- src/requests/tickets.requests.ts
+++ src/requests/tickets.requests.ts
@@ -39,3 +39,13 @@ export const GET_TICKET_INFOS = gql`
     }
   }
 `;
+
+export const UPDATE_TICKET_STATUS = gql`
+  mutation Mutation($updateTicketStatusData: UpdateStatusTicketInput!) {
+    updateTicketStatus(data: $updateTicketStatusData) {
+      id
+      status
+      code
+    }
+  }
+`;
