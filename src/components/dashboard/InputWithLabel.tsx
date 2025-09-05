41aeb4805712ae6cb9452fed4168860cb8f8712b
kaourantinu <hello@corentintournier.fr>
2025-09-05 16:26:27 +0200

    Login page redesign

--- src/components/dashboard/InputWithLabel.tsx
+++ src/components/dashboard/InputWithLabel.tsx
@@ -9,16 +9,34 @@ interface InputWithLabelProps
   readonly error?: string;
 }
 
-function InputWithLabelFunc({ label, id, name, className, error, ...props }: InputWithLabelProps,ref: React.Ref<HTMLInputElement>) {
+function InputWithLabelFunc(
+  { label, id, name, className, error, ...props }: InputWithLabelProps,
+  ref: React.Ref<HTMLInputElement>
+) {
   const inputId = id || name;
 
   return (
-    <div className={cn("flex flex-col gap-1 w-full max-w-sm", className)}>
-      <Label htmlFor={inputId} className={cn("text-sm font-medium text-gray-800", error && "text-red-600")}>
+    <div className={cn("flex flex-col gap-2 w-full", className)}>
+      <Label
+        htmlFor={inputId}
+        className={cn(
+          "text-base font-normal text-gray-800",
+          error && "text-red-600"
+        )}
+      >
         {label} {props.required && "*"}
       </Label>
-      <Input id={inputId} name={name} aria-invalid={!!error} ref={ref} {...props}
-        className={cn(error && "border-red-500 focus-visible:ring-red-500", className)}/>
+      <Input
+        id={inputId}
+        name={name}
+        aria-invalid={!!error}
+        ref={ref}
+        {...props}
+        className={cn(
+          error && "border-red-500 focus-visible:ring-red-500",
+          className
+        )}
+      />
       {error && <p className="text-sm text-red-500 mt-1">{error}</p>}
     </div>
   );
