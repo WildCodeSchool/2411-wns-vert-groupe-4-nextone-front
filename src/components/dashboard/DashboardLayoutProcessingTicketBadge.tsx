db77f00482243e46ecbb2608473775436dc6ebde
kaourantinu <hello@corentintournier.fr>
2025-10-31 18:33:19 +0100

    styling badge and add dropdown menu

--- src/components/dashboard/DashboardLayoutProcessingTicketBadge.tsx
+++ src/components/dashboard/DashboardLayoutProcessingTicketBadge.tsx
@@ -1,36 +1,51 @@
 import { useOperator } from "@/context/OperatorContext";
+import dayjs from "dayjs";
+import duration from "dayjs/plugin/duration";
+import { IoIosMore } from "react-icons/io";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+
+dayjs.extend(duration);
 
 export default function DashboardLayoutProcessingTicketBadge() {
   const { processingTicket, elapsedTimeInSeconds } = useOperator();
-
-  console.log("Rendering ProcessingTicketBadge", {
-    processingTicket,
-    elapsedTimeInSeconds,
-  });
 
   if (!processingTicket) {
     return null;
   }
 
   const formatElapsedTime = (totalSeconds: number) => {
-    const hours = Math.floor(totalSeconds / 3600)
-      .toString()
-      .padStart(2, "0");
-    const minutes = Math.floor((totalSeconds % 3600) / 60)
-      .toString()
-      .padStart(2, "0");
-    const seconds = (totalSeconds % 60).toString().padStart(2, "0");
+    const dur = dayjs.duration(totalSeconds, "seconds");
+    const hours = Math.floor(dur.asHours()).toString().padStart(2, "0");
+    const minutes = dur.minutes().toString().padStart(2, "0");
+    const seconds = dur.seconds().toString().padStart(2, "0");
     return `${hours}:${minutes}:${seconds}`;
   };
 
   return (
-    <div className="fixed bottom-4 right-4 bg-yellow-500 text-black px-4 py-2 rounded shadow-lg flex items-center space-x-3 z-50">
-      <div>
-        <strong>Processing Ticket:</strong> {processingTicket.id}
+    <div className="fixed bottom-6 right-6 bg-white text-black pr-4 pl-2 py-2 rounded-full shadow-lg flex items-center z-50 text-xl gap-6">
+      <div className="font-bold bg-primary px-4 py-2 rounded-full text-white">
+        DER-006
       </div>
-      <div className="ml-4">
-        <strong>Elapsed Time:</strong> {formatElapsedTime(elapsedTimeInSeconds)}
-      </div>
+      <div>{formatElapsedTime(elapsedTimeInSeconds)}</div>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild className="cursor-pointer">
+          <IoIosMore size={20} />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent
+          className="z-50 p-2 bg-card mb-3"
+          side="top"
+          align="end"
+        >
+          <DropdownMenuItem>Marquer le ticket comme traité</DropdownMenuItem>
+          <DropdownMenuItem>Remettre le ticket en attente</DropdownMenuItem>
+          <DropdownMenuItem>Annuler le ticket</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 }
