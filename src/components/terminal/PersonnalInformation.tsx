47d88e3b03a1d3a6bf697f01524c72153c3ebe98
Maeva-RODRIGUES <maeva.rodrigues@lapiscine.pro>
2025-12-06 17:50:53 +0100

    💥 NEW: Add data-testid on PersonnalInformation for e2e tests

--- src/components/terminal/PersonnalInformation.tsx
+++ src/components/terminal/PersonnalInformation.tsx
@@ -7,12 +7,20 @@ import InputWithLabel from "../dashboard/InputWithLabel";
 import { useTicket } from "../../context/useContextTicket";
 import { ChooseServiceProps } from "../../types/terminal";
 import { PersoInfo } from "../../types/terminal";
-import {persoInfo} from "../../utils/validations/personnalInformationValidation"
+import { persoInfo } from "../../utils/validations/personnalInformationValidation";
 
-function PersonnalInformation({ onBack, onNext, onCancel }: ChooseServiceProps) {
+function PersonnalInformation({
+  onBack,
+  onNext,
+  onCancel,
+}: ChooseServiceProps) {
   const { ticket, setTicket } = useTicket();
 
-  const { register, handleSubmit, formState: { errors }} = useForm<PersoInfo>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<PersoInfo>({
     resolver: yupResolver(persoInfo),
     defaultValues: {
       name: ticket.name || "",
@@ -29,13 +37,33 @@ function PersonnalInformation({ onBack, onNext, onCancel }: ChooseServiceProps) 
     <div className="flex flex-col md:flex-row h-screen w-full font-sans bg-white">
       <div className="w-full md:w-1/2 flex flex-col p-4 md:pl-8 overflow-y-auto">
         <div className="sticky top-0 bg-white z-10 pb-4">
-          <Stepper currentStep={2}/>
+          <Stepper currentStep={2} />
         </div>
         <div className="flex-1 flex flex-col justify-center">
-          <form data-testid="form" onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 md:gap-6">
-            <InputWithLabel label="Votre nom ?" placeholder="Nom" error={errors.name?.message}{...register("name")}/>
-            <InputWithLabel label="Votre prénom ?" placeholder="Prénom" error={errors.firstName?.message} {...register("firstName")}/>
-            <NavigationActions onBack={onBack} onCancel={onCancel} />
+          <form
+            data-testid="form"
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col gap-4 md:gap-6"
+          >
+            <InputWithLabel
+              label="Votre nom ?"
+              placeholder="Nom"
+              error={errors.name?.message}
+              {...register("name")}
+              data-testid="lastName-input"
+            />
+            <InputWithLabel
+              label="Votre prénom ?"
+              placeholder="Prénom"
+              error={errors.firstName?.message}
+              {...register("firstName")}
+              data-testid="firstName-input"
+            />
+            <NavigationActions
+              onBack={onBack}
+              onCancel={onCancel}
+              nextButtonTestId="next-button-step-2"
+            />
           </form>
         </div>
       </div>
